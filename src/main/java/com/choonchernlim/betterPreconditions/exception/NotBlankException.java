f22a998619ee4fe8decf83348eb5e82f732db64f
Choon-Chern Lim <limc@mayo.edu>
2015-04-30 14:25:50 -0500

    StringPreconditions.checkNotBlank(..)

---
+++ src/main/java/com/choonchernlim/betterPreconditions/exception/NotBlankException.java
@@ -0,0 +1,8 @@
+package com.choonchernlim.betterPreconditions.exception;
+
+public class NotBlankException extends RuntimeException {
+
+    public NotBlankException(String s) {
+        super(s);
+    }
+}
