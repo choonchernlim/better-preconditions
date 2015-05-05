5b84ebba58c37d29c72ead1498df56ff44e2a276
Choon-Chern Lim <limc@mayo.edu>
2015-05-05 15:41:07 -0500

    Make PreconditionException public

--- src/main/java/com/choonchernlim/betterPreconditions/exception/PreconditionException.java
+++ src/main/java/com/choonchernlim/betterPreconditions/exception/PreconditionException.java
@@ -3,14 +3,14 @@ package com.choonchernlim.betterPreconditions.exception;
 /**
  * Abstract class for all precondition exceptions.
  */
-public abstract class PreconditionException extends RuntimeException {
+public class PreconditionException extends RuntimeException {
 
     /**
      * Constructs a new exception with an error message.
      *
      * @param error Error message
      */
-    protected PreconditionException(String error) {
+    public PreconditionException(String error) {
         super(error);
     }
 }
